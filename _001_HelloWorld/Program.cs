ab8ea5296b52977e72e6f9b8a25fe4b63b18c45b
MapleInori <572569521@qq.com>
2025-06-17 12:02:43 +0800

    添加项目文件。

---
+++ _001_HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _001_HelloWorld   //引用系统命名空间
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Hello world";                  //控制台窗体标题
+            Console.WriteLine("Hello world");               //在控制台窗口中输出hello world
+            Console.ReadLine();                             //等待从控制台输入一行字符串
+        }
+    }
+}
