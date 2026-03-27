04641529975d3bd3368832ebb40636c86cff5d86
MapleInori <572569521@qq.com>
2026-03-27 19:51:05 +0800

    165

---
+++ Chapter_14/_163_FileCombine/Program.cs
@@ -0,0 +1,27 @@
+namespace _163_FileCombine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = Directory.GetCurrentDirectory() + "\\300 classic examples.txt";
+            FileStream file = File.Create(fileName);                //创建主文件
+            int subIndex = 0;                                            //子文件索引
+            while (File.Exists(file.Name + "." + subIndex + ".segment"))
+            {
+                //打开子文件流
+                FileStream subFile = new FileStream(file.Name + "." + subIndex + ".segment",
+                                                    FileMode.Open);
+                byte[] buffer = new byte[subFile.Length];                //子文件缓存
+                int readLength = subFile.Read(buffer, 0, buffer.Length); //读取子文件
+                file.Write(buffer, 0, readLength);                   //将子文件写入主文件中
+                subFile.Close();
+                subIndex++;
+            }
+            file.Close();                                           //关闭主文件流
+            StreamReader reader = new StreamReader(fileName);
+            Console.WriteLine(reader.ReadToEnd());                  //输出拼接后的文件
+            reader.Close();
+        }
+    }
+}
---
+++ Common/PathHelper.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+
+namespace Common;
+
+public static class PathHelper
+{
+    public static string GetSolutionRoot()
+    {
+        DirectoryInfo? dir = new DirectoryInfo(AppContext.BaseDirectory);
+        while (dir != null)
+        {
+            if (dir.GetFiles("*.sln").Length > 0)
+            {
+                return dir.FullName;
+            }
+            dir = dir.Parent;
+        }
+        throw new DirectoryNotFoundException("Solution root not found.");
+    }
+
+    public static string GetDataDirectory()
+    {
+        return Path.Combine(GetSolutionRoot(), "data");
+    }
+
+    public static void EnsureDataDirectory()
+    {
+        Directory.CreateDirectory(GetDataDirectory());
+    }
+
+    public static void SetCurrentDirectoryToSolutionRoot()
+    {
+        Directory.SetCurrentDirectory(GetSolutionRoot());
+    }
+}
